eb80c0d131bc0c726e183036e2904d87f2ddffc8
Mostafa Khavari <web.khavari@gmail.com>
2020-04-02 01:11:11 +0430

    split head tag from header component

--- Component/Header.php
+++ Component/Header.php
@@ -1,17 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/style.css">
-
-    <title>first-practice</title>
-</head>
-<body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Logo</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -36,5 +23,3 @@
 
     </div>
 </nav>
-</body>
-</html>
