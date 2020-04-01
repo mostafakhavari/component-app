7f164acaf596fea6b840ede5fe3bc89ddb16dc40
Mostafa Khavari <web.khavari@gmail.com>
2020-04-01 14:40:30 +0430

    add first-practice to git

---
+++ Component/Footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+    </div>
+</footer>
+<script src="./js/jquery-3.4.1.min.js"></script>
+<script src="./js/bootstrap.min.js"></script>
+
---
+++ index.php
@@ -0,0 +1,17 @@
+
+<body>
+    <?php
+    include_once ('./Component/Header.php');
+    ?>
+
+    <br>
+    <?php
+    include_once ('./Component/Slider.php');
+    ?>
+    <br>
+
+    <?php
+    include_once ('./Component/Footer.php');
+    ?>
+</body>
+</html>
